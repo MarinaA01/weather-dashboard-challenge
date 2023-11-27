cf82ff4190a7ed543c7f3f9a977fc42594830918
Marina <marinajalonzo1@gmail.com>
2023-11-27 02:15:09 -0600

    JS starter layout

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+var searchformEl = $('#search-form');
+
+function handleSearchFormSubmit(event) {
+    event.preventDefault();
+
+
+}
+
+searchformEl.addEventListener('submit', handleSearchFormSubmit);
