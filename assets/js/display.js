e7c21dc056ae51bd6f7c152175ff8a2cd749a95c
Marina <marinajalonzo1@gmail.com>
2023-11-27 15:54:18 -0600

    JS fixes

--- assets/js/display.js
+++ assets/js/display.js
@@ -1,2 +1,9 @@
-today = dayjs();
+var today = dayjs();
+var currentDate = $('#currentDay');
 
+function getCurrentDate() {
+    $('#currentDay').innerHTML = today.format('[YYYYescape] MM/DD/YYYY');
+
+}
+
+getCurrentDate()
