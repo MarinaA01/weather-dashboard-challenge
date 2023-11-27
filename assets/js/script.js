1888fb0283bf80e5393c60c6e64f7592ba902929
Marina <marinajalonzo1@gmail.com>
2023-11-27 02:52:51 -0600

    starter JS.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,23 @@
+fetch(
+    'api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=0806a7d4dcf9a0279005b42384c3dda9'
+)
+
 var searchformEl = $('#search-form');
 
 function handleSearchFormSubmit(event) {
     event.preventDefault();
+    searchHistory();
 
+    $('#search-input').val(localStorage.getItem)
 
 }
+
+function searchHistory() {
+    const task = $('#search-input').val();
+    const input = $(this).parent().attr('id');
+    localStorage.setItem(input, task);
+}
+
+
 
 searchformEl.addEventListener('submit', handleSearchFormSubmit);
