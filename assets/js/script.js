f1d4ad34d381e7aefec1ed32c121aecf709ee3ff
Marina <marinajalonzo1@gmail.com>
2023-11-27 15:02:48 -0600

    edits.

--- assets/js/script.js
+++
@@ -1,9 +0,0 @@
-fetch(
-    'http://api.openweathermap.org/geo/1.0/direct?q=Orlando,FL,USA&limit=5&appid=2ee898c012e56cb36166d4a71be09113'
-)
-.then(function(response) {
-    return response.json();
-})
-.then(function (data) {
-    console.log(data);
-})
